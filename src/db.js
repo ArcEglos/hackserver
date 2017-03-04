5e6fb23fccc0b44565b45b2f9a356c2904aa0b96
extronics <post@calitrix.de>
2017-03-04 17:43:28 +0100

    add db config

--- src/db.js
+++ src/db.js
@@ -1,18 +1,7 @@
 import mongoose from 'mongoose'
 
-export default callback => {
-  let user = process.env.DB_USER
-  let password = process.env.DB_PASSWORD
-  let db = process.env.DB_NAME || 'finder'
-  let host = process.env.DB_HOST || 'localhost'
-  let port = process.env.DB_PORT || '27017'
-  let credentials = ''
-
-  if (user || password) {
-    credentials = `${user}:${password}@`
-  }
-
-  mongoose.connect(`mongodb://${credentials}${host}:${port}/${db}`)
+export default (config, callback) => {
+  mongoose.connect(config.db.url)
 
 	// connect to a database if needed, then pass it to `callback`:
 	callback()
