558b4713c31043e9a60f0ea5239d78418535e50d
Florian Scholz <scholz@heartbeat-med.de>
2017-03-05 03:14:22 +0100

    points in account

--- src/api/account.js
+++ src/api/account.js
@@ -1,22 +1,48 @@
 import resource from 'resource-router-middleware';
 import Account from '../models/account';
 
-export default ({ config, db }) => resource({
+const actionRewards = {SHARE: 200, ADD_FRIEND: 50};
 
-	/** Property name to store preloaded entity on `request`. */
-	id : 'account',
-
-	/** POST / - Create a new entity */
-	create({ body: {userId, accessToken} }, res) {
+export default ({config, db}) => resource({
+  /** Property name to store preloaded entity on `request`. */
+  id: 'account',
+  /** POST / - Create a new entity */
+  create({body: {userId, accessToken}}, res) {
+    Account
+      .findOne({userId})
+      .then(account => {
+        if (account) return account.update({accessToken});
+        return Account.create({userId, accessToken, points: 0});
+      })
+      .then(
+        account => {
+          res.status(200).send(account);
+        },
+        err => {
+          console.error(err);
+          res.status(500);
+        }
+      );
+  },
+  update({body: {userId, actions}}, res) {
     Account.findOne({userId}).then(account => {
-      if (account) return account.update({accessToken})
-      return Account.create({userId, accessToken})
-    })
-    .then(account => {
-      res.status(200).send(account)
-    }, err => {
-      console.error(err)
-      res.status(500)
-    })
-	}
-})
+      if (account) {
+        const newPoints = account.points +
+          actions.reduce(
+            (sum, actionName) => sum + actionRewards[actionName],
+            0
+          );
+        account
+          .update({points: newPoints})
+          .then(() => {
+            res.status(200).send({newPoints});
+          })
+          .catch(() => {
+            res.status(500);
+          });
+      } else {
+        res.status(500);
+      }
+    });
+  },
+});
